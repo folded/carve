aea45d798f1b83c2830f90b748637a6c34c21957
Tobias Sargeant <tobias.sargeant@gmail.com>
2009-11-24 14:45:00 +1100

    moved collection types that don't form part of the interface into private headers

---
+++ include/carve/csg_triangulator.hpp
@@ -0,0 +1,235 @@
+// Begin License:
+// Copyright (C) 2006-2008 Tobias Sargeant (tobias.sargeant@gmail.com).
+// All rights reserved.
+//
+// This file is part of the Carve CSG Library (http://carve-csg.com/)
+//
+// This file may be used under the terms of the GNU General Public
+// License version 2.0 as published by the Free Software Foundation
+// and appearing in the file LICENSE.GPL2 included in the packaging of
+// this file.
+//
+// This file is provided "AS IS" with NO WARRANTY OF ANY KIND,
+// INCLUDING THE WARRANTIES OF DESIGN, MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE.
+// End:
+
+#include <carve/csg.hpp>
+#include <carve/tag.hpp>
+#include <carve/triangulator.hpp>
+
+namespace carve {
+  namespace csg {
+
+    namespace detail {
+      template<bool with_improvement>
+      class CarveTriangulator : public csg::CSG::Hook {
+
+      public:
+        CarveTriangulator() {
+        }
+
+        virtual ~CarveTriangulator() {
+        }
+
+        virtual void processOutputFace(std::vector<poly::Polyhedron::face_t *> &faces,
+                                       const poly::Polyhedron::face_t *orig,
+                                       bool flipped) {
+          std::vector<poly::Polyhedron::face_t *> out_faces;
+
+          size_t n_tris = 0;
+          for (size_t f = 0; f < faces.size(); ++f) {
+            ASSERT(faces[f]->vertices.size() >= 3);
+            n_tris += faces[f]->vertices.size() - 2;
+          }
+
+          out_faces.reserve(n_tris);
+
+          for (size_t f = 0; f < faces.size(); ++f) {
+            poly::Polyhedron::face_t *face = faces[f];
+
+            if (face->vertices.size() == 3) {
+              out_faces.push_back(face);
+              continue;
+            }
+
+            std::vector<triangulate::tri_idx> result;
+
+            triangulate::triangulate(poly::p2_adapt_project<3>(face->project), face->vertices, result);
+            if (with_improvement) {
+              triangulate::improve(poly::p2_adapt_project<3>(face->project), face->vertices, result);
+            }
+
+            std::vector<const poly::Polyhedron::vertex_t *> fv;
+            fv.resize(3);
+            for (size_t i = 0; i < result.size(); ++i) {
+              fv[0] = face->vertices[result[i].a];
+              fv[1] = face->vertices[result[i].b];
+              fv[2] = face->vertices[result[i].c];
+              out_faces.push_back(face->create(fv, false));
+            }
+            delete face;
+          }
+          std::swap(faces, out_faces);
+        }
+      };
+    }
+
+    typedef detail::CarveTriangulator<false> CarveTriangulator;
+    typedef detail::CarveTriangulator<true> CarveTriangulatorWithImprovement;
+
+    class CarveTriangulationImprover : public csg::CSG::Hook {
+    public:
+      CarveTriangulationImprover() {
+      }
+
+      virtual ~CarveTriangulationImprover() {
+      }
+
+      virtual void processOutputFace(std::vector<poly::Polyhedron::face_t *> &faces,
+                                     const poly::Polyhedron::face_t *orig,
+                                     bool flipped) {
+        if (faces.size() == 1) return;
+
+        // doing improvement as a separate hook is much messier than
+        // just incorporating it into the triangulation hook.
+
+        typedef std::map<const poly::Polyhedron::vertex_t *, size_t> vert_map_t;
+        std::vector<poly::Polyhedron::face_t *> out_faces;
+        vert_map_t vert_map;
+
+        out_faces.reserve(faces.size());
+
+        poly::p2_adapt_project<3> projector(faces[0]->project);
+
+        std::vector<triangulate::tri_idx> result;
+
+        for (size_t f = 0; f < faces.size(); ++f) {
+          poly::Polyhedron::face_t *face = faces[f];
+          if (face->vertices.size() != 3) {
+            out_faces.push_back(face);
+          } else {
+            triangulate::tri_idx tri;
+            for (size_t i = 0; i < 3; ++i) {
+              size_t v = 0;
+              vert_map_t::iterator j = vert_map.find(face->vertices[i]);
+              if (j == vert_map.end()) {
+                v = vert_map.size();
+                vert_map[face->vertices[i]] = v;
+              } else {
+                v = (*j).second;
+              }
+              tri.v[i] = v;
+            }
+            result.push_back(tri);
+            delete face;
+          }
+        }
+
+        std::vector<const poly::Polyhedron::vertex_t *> verts;
+        verts.resize(vert_map.size());
+        for (vert_map_t::iterator i = vert_map.begin(); i != vert_map.end(); ++i) {
+          verts[(*i).second] = (*i).first;
+        }
+ 
+        triangulate::improve(projector, verts, result);
+
+        std::vector<const poly::Polyhedron::vertex_t *> fv;
+        fv.resize(3);
+        for (size_t i = 0; i < result.size(); ++i) {
+          fv[0] = verts[result[i].a];
+          fv[1] = verts[result[i].b];
+          fv[2] = verts[result[i].c];
+          out_faces.push_back(orig->create(fv, false));
+        }
+
+        std::swap(faces, out_faces);
+      }
+    };
+
+    class CarveTriangulationQuadMerger : public csg::CSG::Hook {
+      typedef std::map<V2, F2> edge_map_t;
+
+    public:
+      CarveTriangulationQuadMerger() {
+      }
+
+      virtual ~CarveTriangulationQuadMerger() {
+      }
+
+      double scoreQuad(edge_map_t::iterator i, edge_map_t &edge_map) {
+        if (!(*i).second.first || !(*i).second.second) return -1;
+      }
+
+      poly::Polyhedron::face_t *mergeQuad(edge_map_t::iterator i, edge_map_t &edge_map) {
+      }
+
+      void recordEdge(const poly::Polyhedron::vertex_t *v1,
+                      const poly::Polyhedron::vertex_t *v2,
+                      const poly::Polyhedron::face_t *f,
+                      edge_map_t &edge_map) {
+        if (v1 < v2) {
+          edge_map[V2(v1, v2)].first = f;
+        } else {
+          edge_map[V2(v2, v1)].second = f;
+        }
+      }
+
+      virtual void processOutputFace(std::vector<poly::Polyhedron::face_t *> &faces,
+                                     const poly::Polyhedron::face_t *orig,
+                                     bool flipped) {
+        if (faces.size() == 1) return;
+
+        std::vector<poly::Polyhedron::face_t *> out_faces;
+        edge_map_t edge_map;
+
+        out_faces.reserve(faces.size());
+
+        poly::p2_adapt_project<3> projector(faces[0]->project);
+
+        for (size_t f = 0; f < faces.size(); ++f) {
+          poly::Polyhedron::face_t *face = faces[f];
+          if (face->vertices.size() != 3) {
+            out_faces.push_back(face);
+          } else {
+            recordEdge(face->vertices[0], face->vertices[1], face, edge_map);
+            recordEdge(face->vertices[1], face->vertices[2], face, edge_map);
+            recordEdge(face->vertices[2], face->vertices[0], face, edge_map);
+          }
+        }
+
+        for (edge_map_t::iterator i = edge_map.begin(); i != edge_map.end();) {
+          if ((*i).second.first && (*i).second.second) {
+            ++i;
+          } else {
+            edge_map.erase(i++);
+          }
+        }
+
+        while (edge_map.size()) {
+          edge_map_t::iterator i = edge_map.begin();
+          edge_map_t::iterator best = i;
+          double best_score = scoreQuad(i, edge_map);
+          for (++i; i != edge_map.end(); ++i) {
+            double score = scoreQuad(i, edge_map);
+            if (score > best_score) best = i;
+          }
+          if (best_score < 0) break;
+          out_faces.push_back(mergeQuad(best, edge_map));
+        }
+
+        if (edge_map.size()) {
+          tagable::tag_begin();
+          for (edge_map_t::iterator i = edge_map.begin(); i != edge_map.end(); ++i) {
+            poly::Polyhedron::face_t *a = const_cast<poly::Polyhedron::face_t *>((*i).second.first);
+            poly::Polyhedron::face_t *b = const_cast<poly::Polyhedron::face_t *>((*i).second.first);
+            if (a && a->tag_once()) out_faces.push_back(a);
+            if (b && b->tag_once()) out_faces.push_back(b);
+          }
+        }
+
+        std::swap(faces, out_faces);
+      }
+    };
+  }
+}
---
+++ include/carve/geom3d.hpp
@@ -0,0 +1,185 @@
+// Begin License:
+// Copyright (C) 2006-2008 Tobias Sargeant (tobias.sargeant@gmail.com).
+// All rights reserved.
+//
+// This file is part of the Carve CSG Library (http://carve-csg.com/)
+//
+// This file may be used under the terms of the GNU General Public
+// License version 2.0 as published by the Free Software Foundation
+// and appearing in the file LICENSE.GPL2 included in the packaging of
+// this file.
+//
+// This file is provided "AS IS" with NO WARRANTY OF ANY KIND,
+// INCLUDING THE WARRANTIES OF DESIGN, MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE.
+// End:
+
+
+#pragma once
+
+#include <carve/carve.hpp>
+#include <carve/geom.hpp>
+
+#include <math.h>
+#include <carve/math_constants.hpp>
+
+#include <vector>
+#include <list>
+#include <map>
+
+namespace carve {
+  namespace geom3d {
+
+    typedef carve::geom::plane<3> Plane;
+    typedef carve::geom::ray<3> Ray;
+    typedef carve::geom::linesegment<3> LineSegment;
+    typedef carve::geom::vector<3> Vector;
+
+    template<typename iter_t, typename adapt_t>
+    bool fitPlane(iter_t begin, iter_t end, adapt_t adapt, Plane &plane) {
+      Vector centroid;
+      carve::geom::centroid(begin, end, adapt, centroid);
+      iter_t i;
+
+      Vector n = Vector::ZERO();
+      Vector v, z;
+      Vector p1, p2, p3, c1, c2;
+      if (begin == end) return false;
+
+      i = begin;
+      p1 = c1 = adapt(*i++); if (i == end) return false;
+      p2 = c2 = adapt(*i++); if (i == end) return false;
+
+#if defined(DEBUG)
+      size_t N = 2;
+#endif
+      while (i != end) {
+        p3 = adapt(*i++);
+        v = cross(p3 - p2, p1 - p2);
+        if (v.v[dominantAxis(v)]) v.negate();
+        n += v;
+        p1 = p2; p2 = p3;
+#if defined(DEBUG)
+        ++N;
+#endif
+      }
+
+      p1 = p2; p2 = p3; p3 = c1;
+      v = cross(p3 - p2, p1 - p2);
+      if (v.v[dominantAxis(v)]) v.negate();
+      n += v;
+
+      p1 = p2; p2 = p3; p3 = c2;
+      v = cross(p3 - p2, p1 - p2);
+      if (v.v[dominantAxis(v)]) v.negate();
+      n += v;
+
+      n.normalize();
+      plane.N = n;
+      plane.d = -dot(n, centroid);
+#if defined(DEBUG)
+      if (N > 3) {
+        std::cerr << "N = " << N << " fitted distance:";
+        for (i = begin; i != end; ++i) {
+          Vector p = adapt(*i);
+          std::cerr << " {" << p << "} " << distance(plane, p);
+        }
+        std::cerr << std::endl;
+      }
+#endif
+      return true;
+    }
+
+    bool planeIntersection(const Plane &a, const Plane &b, Ray &r);
+
+    IntersectionClass rayPlaneIntersection(const Plane &p,
+                                           const Vector &v1,
+                                           const Vector &v2,
+                                           Vector &v,
+                                           double &t);
+
+    IntersectionClass lineSegmentPlaneIntersection(const Plane &p,
+                                                   const LineSegment &line,
+                                                   Vector &v);
+
+    RayIntersectionClass rayRayIntersection(const Ray &r1,
+                                            const Ray &r2,
+                                            Vector &v1,
+                                            Vector &v2,
+                                            double &mu1,
+                                            double &mu2);
+
+
+
+    // test whether point d is above, below or on the plane formed by the triangle a,b,c.
+    // return: +ve = d is below a,b,c
+    //         -ve = d is above a,b,c
+    //           0 = d is on a,b,c
+    static inline double orient3d(const carve::geom3d::Vector &a,
+        const carve::geom3d::Vector &b,
+        const carve::geom3d::Vector &c,
+        const carve::geom3d::Vector &d) {
+      return dotcross((a - d), (b - d), (c - d));
+    }
+
+
+
+    // Volume of a tetrahedron described by 4 points. Will be
+    // positive if the anticlockwise normal of a,b,c is oriented out
+    // of the tetrahedron.
+    //
+    // see: http://mathworld.wolfram.com/Tetrahedron.html
+    static inline double tetrahedronVolume(const carve::geom3d::Vector &a,
+        const carve::geom3d::Vector &b,
+        const carve::geom3d::Vector &c,
+        const carve::geom3d::Vector &d) {
+      return dotcross((a - d), (b - d), (c - d)) / 6.0;
+    }
+
+
+
+    // The anticlockwise angle from vector "from" to vector "to", oriented around the vector "orient".
+    static inline double antiClockwiseAngle(const Vector &from, const Vector &to, const Vector &orient) {
+      double dp = dot(from, to);
+      Vector cp = cross(from, to);
+      if (cp.isZero()) {
+        if (dp < 0) {
+          return M_PI;
+        } else {
+          return 0.0;
+        }
+      } else {
+        if (dot(cp, orient) > 0.0) {
+          return acos(dp);
+        } else {
+          return M_TWOPI - acos(dp);
+        }
+      }
+    }
+
+
+
+    static inline double antiClockwiseOrdering(const Vector &from, const Vector &to, const Vector &orient) {
+      double dp = dot(from, to);
+      Vector cp = cross(from, to);
+      if (cp.isZero()) {
+        if (dp < 0) {
+          return 2.0;
+        } else {
+          return 0.0;
+        }
+      } else {
+        if (dot(cp, orient) > 0.0) {
+          // 1..-1 -> 0..2
+          return 1.0 - dp;
+        } else {
+          // -1..1 -> 2..4
+          return dp + 1.0;
+        }
+      }
+    }
+
+
+
+  }
+}
