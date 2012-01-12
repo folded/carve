cbfcaf806751147c3a6080d50a8f46fceedb6524
Tobias Sargeant <tobias.sargeant@gmail.com>
2012-01-12 10:46:36 -0500

    add test for compilation issues resulting from 'using namespace std'.
    this issue is fixed by changeset 5c227171859d.

---
+++ tests/stdlib_conflict.cpp
@@ -0,0 +1,11 @@
+#include <carve/carve.hpp>
+#include <carve/input.hpp>
+#include <carve/csg_triangulator.hpp>
+
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  string myString;
+}
