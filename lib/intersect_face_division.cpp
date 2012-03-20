e05b1eeedbc88d306a418d8d516965063f12ce5e
Tobias Sargeant <tobias.sargeant@gmail.com>
2012-03-20 09:49:32 -0400

    Fix writePLY prototypes.

--- lib/intersect_face_division.cpp
+++ lib/intersect_face_division.cpp
@@ -40,6 +40,12 @@ typedef carve::poly::Polyhedron poly_t;
 
 
 
+#if defined(CARVE_DEBUG_WRITE_PLY_DATA)
+void writePLY(const std::string &out_file, const carve::line::PolylineSet *lines, bool ascii);
+#endif
+
+
+
 namespace {
 
 
